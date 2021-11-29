acc94c28950a3a1b55032ffbf9c52fd895417c13
Kevin <kevinkamacho47@gmail.com>
2021-11-29 02:30:24 -0700

    Añadir BottomNavigation

--- app/src/main/java/com/uas/agenda/activities/MenuActivity.kt
+++ app/src/main/java/com/uas/agenda/activities/MenuActivity.kt
@@ -4,10 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import androidx.navigation.ui.*
 import com.google.android.material.navigation.NavigationView
 import com.uas.agenda.R
 import com.uas.agenda.databinding.ActivityMenuBinding
@@ -29,22 +26,24 @@ class MenuActivity : AppCompatActivity() {
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_menu)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+        navView.setupWithNavController(navController)
+
+
         appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.nav_dates, R.id.nav_status, R.id.nav_slideshow
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+
+        NavigationUI.setupWithNavController(binding.bottomNavView, navController)
 
         val info: Bundle? = intent.extras
 
         val headerView = navView.getHeaderView(0)
         val headerBinding = NavHeaderMenuBinding.bind(headerView)
 
-        headerBinding.textViewUser.text = "Bienvenido"
+        headerBinding.textViewUser.text = getString(R.string.navDrawerTitle)
 
         if (info != null) {
             headerBinding.textViewUserEmail.text = info.getString("correo")
